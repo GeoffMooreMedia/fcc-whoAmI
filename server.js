461ec43effeeba19de9fe12982b53b14d4057927
Glitch (fcc-whoami-microservice) <none>
2018-04-01 21:40:28 +0000

    Finished Project

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var getIP = require('ipware')().get_ip;
 
 app.get('/', function(req, res){
   var userObj = {};
+  var userAgent = req.get('User-Agent').split('(')[1];
   userObj['ipaddress'] = getIP(req).clientIp;
+  userObj['language'] = req.headers['accept-language'];
+  userObj['software'] = userAgent.split(')')[0];
    res.send(userObj); 
 });
 
